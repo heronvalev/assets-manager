0e5c245f7ac9ff1b7a20943d74054bf346fc80c4
Heron <c.heron.22@outlook.com>
2025-09-09 14:23:53 +0100

    Register Asset and Assignment models in Django admin

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Asset, Assignment
 
-# Register your models here.
+@admin.register(Asset)
+class AssetAdmin(admin.ModelAdmin):
+    list_display = ('name', 'serial_number', 'category', 'status', 'location')
+
+@admin.register(Assignment)
+class AssignmentAdmin(admin.ModelAdmin):
+    list_display = ('asset', 'assigned_upn', 'assigned_date', 'returned_date')
