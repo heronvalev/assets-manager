7db8c665a2c811293f94e3d151fea03448b8265f
Heron <c.heron.22@outlook.com>
2025-09-16 13:51:42 +0100

    Add Assets list view with templates and base layout

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Asset
 
-# Create your views here.
+def asset_list(request):
+    """
+    Display a list of all assets with basic info.
+    """
+    assets = Asset.objects.all().order_by('name')
+    
+    context = {
+        'assets': assets
+    }
+    return render(request, 'inventory/asset_list.html', context)
