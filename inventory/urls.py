a9b00b9b87df01807b09d2c21d22684c771a580a
Heron <c.heron.22@outlook.com>
2025-09-16 15:56:43 +0100

    Add assignments list view and template

--- inventory/urls.py
+++ inventory/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('assets/', views.asset_list, name='asset_list'),
+    path('assignments', views.assignment_list, name='assignment_list'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Asset
 
+# Assets
 def asset_list(request):
     """
     Display a list of all assets with basic info.
@@ -11,3 +12,18 @@ def asset_list(request):
         'assets': assets
     }
     return render(request, 'inventory/asset_list.html', context)
+
+# Assignments
+from django.shortcuts import render
+from .models import Assignment
+
+def assignment_list(request):
+    """
+    Display a list of all assignments, active and historical.
+    """
+    assignments = Assignment.objects.select_related('asset', 'entra_user').order_by('-assigned_date')
+    
+    context = {
+        'assignments': assignments
+    }
+    return render(request, 'inventory/assignment_list.html', context)
